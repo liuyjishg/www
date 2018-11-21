385d9ac7af8e20a839019d3bd6bb7fa880ac6046
Kevin <409216159@qq.com>
2018-11-21 15:48:17 +0800

    modify c

--- cgi-bin/checkbox.py
+++ cgi-bin/checkbox.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import cgi,cgitb
 
-from = cgi.FieldStorage()
+form = cgi.FieldStorage()
 
 if form.getvalue('google')
     google_flag = "是"
@@ -15,6 +15,7 @@ else:
 
 print("Content-type:text/html")
 print()
+print()
 print("<html>")
 print("<head>")
 print("<meta charset=\"utf-8\">")
@@ -22,7 +23,9 @@ print("<title>Python3 Test</title>")
 print("</head>")
 print("<body>")
 print("<h2>youj 选择了: %s </h2>"%youj_flag)
+print()
 print("<h2>Google 选择了: %s </h2>"%google_flag)
 print("</body>")
 print("</html>")
+print()
 
