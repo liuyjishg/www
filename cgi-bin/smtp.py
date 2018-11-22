789ed67dcf7bd9404af4ebf03704cd11db4feeaf
Kevin <409216159@qq.com>
2018-11-22 15:50:03 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

--- cgi-bin/smtp.py
+++ cgi-bin/smtp.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
+
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'from@test.cn'
-receivers = ['409216159@qq.com']
+sender = 'from@runoob.com'
+receivers = ['409216159@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-message = MIMEText('Python TEST mail...','plain','utf-8')
-message['From'] = Header("邮件1",'utf-8')
-message['To'] = Header("测试",'utf-8')
+# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
+message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
+message['From'] = Header("菜鸟教程", 'utf-8')  # 发送者
+message['To'] = Header("测试", 'utf-8')  # 接收者
 
-subject = 'Python SMTP test'
-message['Subject'] = Header(subject,'utf-8')
+subject = 'Python SMTP 邮件测试'
+message['Subject'] = Header(subject, 'utf-8')
 
 try:
     smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender,receivers,message.as_string())
+    smtpObj.sendmail(sender, receivers, message.as_string())
     print("邮件发送成功")
-
 except smtplib.SMTPException:
-    print("Error: 无法发送")
+    print("Error: 无法发送邮件")
