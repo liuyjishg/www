3ca3e801716c81d945ed2e508d33300d4a50cf04
Kevin <409216159@qq.com>
2018-11-22 10:53:44 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

---
+++ cgi-bin/save_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import  cgi,os
+import  cgitb; cgitb.enable()
+
+form = cgi.FieldStorage()
+
+fileitem = form['filename']
+
+if fileitem.filename:
+    fn = os.path.basename(fileitem.filename)
+    open('/tmp/'+ fn,'wb').write(fileitem.file.read())
+
+    message = '文件 "' + fn + '" 上传成功'
+
+else:
+    message = "文件没有上传"
+
+print("""\
+Content-Type: text/html\n
+<html>
+<head>
+<meta charset="utf-8">
+<title> Python 3 </title>
+</head>
+<body>
+    <p>%s</p>
+</body>
+</html>
+"""%(message))
