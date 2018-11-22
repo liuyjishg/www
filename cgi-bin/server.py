2ced3a4d1d4cf3b6b5a1f0b01b45cdea6617ad24
Kevin <409216159@qq.com>
2018-11-22 15:18:21 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

---
+++ cgi-bin/server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import socket
+import sys
+
+serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 9999
+
+serversocket.bind((host,port))
+serversocket.listen(5)
+
+while True:
+    clientsocket,addr = serversocket.accept()
+
+    print("连接地址: %s" %str(addr))
+    msg = 'Python3'+"\r\n"
+    clientsocket.send(msg.encode("utf-8"))
+    clientsocket.close()
+
