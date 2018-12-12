933fec64e0aaa9b03fe2aa08611c64d88d82a373
Kevin <409216159@qq.com>
2018-12-12 16:42:21 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

--- cgi-bin/pa1.py
+++ cgi-bin/pa1.py
@@ -2,7 +2,7 @@
 # -*- coding:UTF-8 -*-
 
 import requests
-if __name__ == '__man__'
+if __name__ == '__man__':
     target = 'http://www.biqukan.com/1_1094/5403177.html'
     req = requests.get(url=target)
     print(req.text)
