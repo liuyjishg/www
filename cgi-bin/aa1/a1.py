41550b76c101667efbd33bc927be2eb576115cb4
Kevin <409216159@qq.com>
2018-12-19 17:14:45 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

--- cgi-bin/aa1/a1.py
+++ cgi-bin/aa1/a1.py
@@ -1,8 +1,11 @@
 # _*_ coding:UTF-8 _*_
 
+from bs4 import BeautifulSoup
 import requests
 
 if __name__ == '__main__':
     target = 'http://www.biqukan.com/1_1094/5403177.html'
     req = requests.get(url=target)
-    print(req.text)
+    html = req.text
+    bf = BeautifulSoup(html)
+    tests = bf.find_all('div',class_='showtxt') print(tests)
