eaf3639c85da9673124fa193e914c30cc3b8af24
Kevin <409216159@qq.com>
2018-12-19 17:09:44 +0800

    add file
    Signed-off-by: Kevin <409216159@qq.com>

---
+++ cgi-bin/aa1/a1.py
@@ -0,0 +1,8 @@
+# _*_ coding:UTF-8 _*_
+
+import requests
+
+if __name__ == '__main__':
+    target = 'http://www.biqukan.com/1_1094/5403177.html'
+    req = requests.get(url=target)
+    print(req.text)
